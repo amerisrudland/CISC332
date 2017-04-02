4e35bdc8f91abb8bc0eaeda6e6ccdbc5346b94cd
Ameris Rudland <13akar@queensu.ca>
2017-04-02 14:36:35 -0400

    testing connection with root user, see connection.php

---
+++ pages/config/connection.php
@@ -0,0 +1,15 @@
+<?php
+ // used to connect to the database
+ $host = "localhost";
+ $user = "root";
+ $password = "toor";
+ $database = "farmers market";
+ try {
+    $con = new mysqli($host,$username,$password, $database);
+}
+ 
+// show error
+catch(Exception $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
+?>
