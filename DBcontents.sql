e9156d5ae847f4f556a5f4e2543f9892aab4f55e
sarahc13 <sarahcolquhoun4@gmail.com>
2017-04-02 22:06:36 -0400

    Update DBcontents.sql

--- DBcontents.sql
+++ DBcontents.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2017 at 11:34 PM
+-- Generation Time: Apr 03, 2017 at 04:05 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `farmers market`
+-- Database: `Market2`
 --
 
 -- --------------------------------------------------------
@@ -36,10 +36,10 @@ CREATE TABLE `fav_products` (
 --
 
 INSERT INTO `fav_products` (`ProdNo`, `PurchaserID`) VALUES
-(12310666, 109030),
-(44567888, 123451),
-(66666888, 123451),
-(88228822, 100023);
+(3, 5),
+(4, 6),
+(6, 6),
+(8, 4);
 
 -- --------------------------------------------------------
 
@@ -62,18 +62,18 @@ CREATE TABLE `foodorder` (
 --
 
 INSERT INTO `foodorder` (`OrderNo`, `ProdNo`, `Quantity_Bought`, `Date_Sold`, `PurchaserID`, `SellerID`, `Delivery_Date`) VALUES
-(7895677, 40000400, 10, '2017-02-28', 109030, 100001, '2017-03-03'),
-(78956778, 40000400, 10, '2017-02-28', 109030, 100001, '2017-03-03'),
-(100005555, 98767787, 15, '2017-02-28', 100023, 425252, '2017-03-03'),
-(101010101, 12334111, 2, '2017-03-02', 100023, 100001, '2017-03-05'),
-(123411111, 66666888, 20, '2017-03-01', 109030, 425252, '2017-03-04'),
-(444444444, 44567888, 15, '2017-02-28', 100023, 100001, '2017-03-03'),
-(555555555, 12310666, 20, '2017-03-01', 109030, 425252, '2017-03-04'),
-(787663267, 88228822, 20, '2017-02-27', 123451, 209202, '2017-03-02'),
-(987652342, 15151515, 25, '2017-02-27', 123451, 209202, '2017-03-02'),
-(999999999, 11234767, 8, '2017-03-01', 100023, 209202, '2017-03-04'),
-(1010101010, 12334111, 2, '2017-03-02', 100023, 100001, '2017-03-05'),
-(2147483647, 44567888, 15, '2017-02-28', 100023, 100001, '2017-03-03');
+(1, 5, 10, '2017-02-28', 5, 1, '2017-03-03'),
+(2, 5, 10, '2017-02-28', 5, 1, '2017-03-03'),
+(3, 9, 15, '2017-02-28', 4, 3, '2017-03-03'),
+(4, 3, 2, '2017-03-02', 4, 1, '2017-03-05'),
+(5, 7, 20, '2017-03-01', 5, 3, '2017-03-04'),
+(6, 6, 15, '2017-02-28', 4, 1, '2017-03-03'),
+(7, 2, 20, '2017-03-01', 5, 3, '2017-03-04'),
+(8, 8, 20, '2017-02-27', 6, 2, '2017-03-02'),
+(9, 4, 25, '2017-02-27', 6, 2, '2017-03-02'),
+(10, 1, 8, '2017-03-01', 4, 2, '2017-03-04'),
+(11, 3, 2, '2017-03-02', 4, 1, '2017-03-05'),
+(12, 6, 15, '2017-02-28', 4, 1, '2017-03-03');
 
 -- --------------------------------------------------------
 
@@ -97,15 +97,15 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`ProdNo`, `Name`, `SellerID`, `Date_Listed`, `Quantity`, `Unit`, `Price_Per_Unit`, `Days_To_Delivery`) VALUES
-(11234767, 'Pork Loin ', 209202, '2017-01-24', 3, '200g', '2.09', 3),
-(12310666, '1% Milk ', 425252, '2017-02-26', 50, '1L ', '3.50', 3),
-(12334111, 'Carrots ', 100001, '2017-03-01', 40, '0.5kg ', '4.00', 3),
-(15151515, 'Chicken Breast ', 209202, '2017-01-25', 20, '300g ', '2.00', 3),
-(40000400, 'Tomatoes ', 100001, '2017-02-04', 200, '1kg ', '5.00', 3),
-(44567888, 'Apples ', 100001, '2017-02-03', 200, '1kg ', '7.00', 3),
-(66666888, '2% Milk ', 425252, '2017-02-25', 50, '1L ', '3.50', 3),
-(88228822, 'Sirloin Steak ', 209202, '2017-01-24', 20, '300g ', '3.10', 3),
-(98767787, 'Chocolate Milk ', 425252, '2017-02-23', 50, '1L ', '3.50', 3);
+(1, 'Pork Loin ', 2, '2017-01-24', 3, '200g', '2.09', 3),
+(2, 'Skim Milk', 3, '2017-04-02', 50, '1L', '3.50', 3),
+(3, 'Carrots ', 1, '2017-03-01', 40, '0.5kg ', '4.00', 3),
+(4, 'Chicken Breast ', 2, '2017-01-25', 20, '300g ', '2.00', 3),
+(5, 'Tomatoes ', 1, '2017-02-04', 200, '1kg ', '5.00', 3),
+(6, 'Apples ', 1, '2017-02-03', 200, '1kg ', '7.00', 3),
+(7, '2% Milk ', 3, '2017-02-25', 50, '1L ', '3.50', 3),
+(8, 'Sirloin Steak ', 2, '2017-01-24', 20, '300g ', '3.10', 3),
+(9, 'Chocolate Milk ', 3, '2017-02-23', 50, '1L ', '3.50', 3);
 
 -- --------------------------------------------------------
 
@@ -114,7 +114,7 @@ INSERT INTO `products` (`ProdNo`, `Name`, `SellerID`, `Date_Listed`, `Quantity`,
 --
 
 CREATE TABLE `purchaser` (
-  `PurchaserID` int(6) NOT NULL,
+  `ID` int(6) NOT NULL,
   `UserName` varchar(15) NOT NULL,
   `Address` varchar(40) NOT NULL,
   `Credit_Card` int(11) NOT NULL
@@ -124,10 +124,10 @@ CREATE TABLE `purchaser` (
 -- Dumping data for table `purchaser`
 --
 
-INSERT INTO `purchaser` (`PurchaserID`, `UserName`, `Address`, `Credit_Card`) VALUES
-(100023, 'MuchoPizzeria', '43 Morris Ave', 900090009),
-(109030, 'Scoops ', '34 Jones Street', 100010001),
-(123451, 'PitaExperience', '10 Victoria Way', 345634563);
+INSERT INTO `purchaser` (`ID`, `UserName`, `Address`, `Credit_Card`) VALUES
+(4, 'MuchoPizzeria', '43 Morris Ave', 900090009),
+(5, 'Scoops ', '34 Jones Street', 100010001),
+(6, 'PitaExperience', '10 Victoria Way', 345634563);
 
 -- --------------------------------------------------------
 
@@ -136,7 +136,7 @@ INSERT INTO `purchaser` (`PurchaserID`, `UserName`, `Address`, `Credit_Card`) VA
 --
 
 CREATE TABLE `seller` (
-  `SellerID` int(6) NOT NULL,
+  `ID` int(6) NOT NULL,
   `UserName` varchar(15) NOT NULL,
   `Address` varchar(40) NOT NULL,
   `Delivery_Method` varchar(20) NOT NULL,
@@ -147,10 +147,34 @@ CREATE TABLE `seller` (
 -- Dumping data for table `seller`
 --
 
-INSERT INTO `seller` (`SellerID`, `UserName`, `Address`, `Delivery_Method`, `Checking_Number`) VALUES
-(100001, 'SmithFarms', '24 Apple Way', 'Truck', 42),
-(209202, 'BlairButchers', '5 Cow Street ', 'Pickup ', 34),
-(425252, 'RudlandDairy', '223 Highway 5', 'Truck ', 90);
+INSERT INTO `seller` (`ID`, `UserName`, `Address`, `Delivery_Method`, `Checking_Number`) VALUES
+(1, 'SmithFarms', '24 Apple Way', 'Truck', 42),
+(2, 'BlairButchers', '5 Cow Street ', 'Pickup ', 34),
+(3, 'RudlandDairy', '223 Highway 5', 'Truck ', 90);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `ID` int(6) NOT NULL,
+  `UserName` varchar(15) NOT NULL,
+  `Password` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`ID`, `UserName`, `Password`) VALUES
+(1, 'SmithFarms', 'SmithFarms'),
+(2, 'BlairButchers', 'BlairButchers'),
+(3, 'RudlandDairy', 'RudlandDairy'),
+(4, 'MuchoPizzeria', 'MuchoPizzeria'),
+(5, 'Scoops', 'Scoops'),
+(6, 'PitaExperience', 'PitaExperience');
 
 --
 -- Indexes for dumped tables
@@ -182,16 +206,42 @@ ALTER TABLE `products`
 -- Indexes for table `purchaser`
 --
 ALTER TABLE `purchaser`
-  ADD PRIMARY KEY (`PurchaserID`),
+  ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `UserName` (`UserName`);
 
 --
 -- Indexes for table `seller`
 --
 ALTER TABLE `seller`
-  ADD PRIMARY KEY (`SellerID`),
+  ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `UserName` (`UserName`);
 
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `UserName` (`UserName`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `foodorder`
+--
+ALTER TABLE `foodorder`
+  MODIFY `OrderNo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProdNo` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `ID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- Constraints for dumped tables
 --
@@ -200,21 +250,33 @@ ALTER TABLE `seller`
 -- Constraints for table `fav_products`
 --
 ALTER TABLE `fav_products`
-  ADD CONSTRAINT `fav_products_ibfk_1` FOREIGN KEY (`PurchaserID`) REFERENCES `purchaser` (`PurchaserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fav_products_ibfk_1` FOREIGN KEY (`PurchaserID`) REFERENCES `purchaser` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fav_products_ibfk_2` FOREIGN KEY (`ProdNo`) REFERENCES `products` (`ProdNo`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `foodorder`
 --
 ALTER TABLE `foodorder`
-  ADD CONSTRAINT `foodorder_ibfk_1` FOREIGN KEY (`PurchaserID`) REFERENCES `purchaser` (`PurchaserID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `foodorder_ibfk_2` FOREIGN KEY (`SellerID`) REFERENCES `seller` (`SellerID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `foodorder_ibfk_1` FOREIGN KEY (`PurchaserID`) REFERENCES `purchaser` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `foodorder_ibfk_2` FOREIGN KEY (`SellerID`) REFERENCES `seller` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`SellerID`) REFERENCES `seller` (`SellerID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`SellerID`) REFERENCES `seller` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `purchaser`
+--
+ALTER TABLE `purchaser`
+  ADD CONSTRAINT `purchaser_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `seller`
+--
+ALTER TABLE `seller`
+  ADD CONSTRAINT `seller_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
