6e53f3d473619ea672d2bfedfd33140a1afcfca3
sarahc13 <sarahcolquhoun4@gmail.com>
2017-04-02 23:10:37 -0400

    Update tablecreation.sql

--- tablecreation.sql
+++ tablecreation.sql
@@ -1,22 +1,32 @@
+CREATE TABLE User(
+	ID  INTEGER(6) NOT NULL AUTO_INCREMENT, 
+	UserName VARCHAR(15) NOT NULL UNIQUE, 
+	Password VARCHAR(15) NOT NULL, 
+	PRIMARY KEY (ID) 
+);
+
 CREATE TABLE Seller(
-    SellerID         INTEGER(6)        NOT NULL,
+    ID         INTEGER(6)       NOT NULL,
     UserName         VARCHAR(15)    NOT NULL UNIQUE,
     Address         VARCHAR(40)    NOT NULL,
     Delivery_Method VARCHAR(20)    NOT NULL,
     Checking_Number INTEGER        NOT NULL,
-    PRIMARY KEY (SellerID)
+    PRIMARY KEY (ID), 
+	FOREIGN KEY (ID) references User(ID) on delete cascade on update cascade
 );
 
 CREATE TABLE Purchaser(
-    PurchaserID     INTEGER(6)        NOT NULL,
+    ID     INTEGER(6)        NOT NULL,
     UserName     VARCHAR(15)    NOT NULL UNIQUE,
     Address         VARCHAR(40)    NOT NULL,
     Credit_Card     INTEGER        NOT NULL,
-    PRIMARY KEY (PurchaserID)
+    PRIMARY KEY (ID), 
+	FOREIGN KEY(ID) references User(ID) on delete cascade on update cascade
 );
 
+
 CREATE TABLE Products(
-    ProdNo             INTEGER(8)        NOT NULL, 
+    ProdNo             INTEGER(8)        NOT NULL AUTO_INCREMENT , 
     Name             VARCHAR(25)    NOT NULL,
     SellerID        INTEGER(6)        NOT NULL,
     Date_Listed     DATE            NOT NULL,
@@ -25,19 +35,19 @@ CREATE TABLE Products(
     Price_Per_Unit    DECIMAL(5,2)     NOT NULL,
 	Days_To_Delivery 	INTEGER   	NOT NULL, 
     PRIMARY KEY (ProdNo), 
-	FOREIGN KEY (SellerID) references Seller(SellerID) on delete cascade on update cascade
+	FOREIGN KEY (SellerID) references Seller(ID) on delete cascade on update cascade
 );
 
 CREATE TABLE Fav_Products(
-    ProdNo             INTEGER(8)        NOT NULL,
+    ProdNo             INTEGER(8)        NOT NULL  ,
     PurchaserID      INTEGER(6)        NOT NULL, 
 	PRIMARY KEY (ProdNo, PurchaserID), 
-	FOREIGN KEY (PurchaserID) references Purchaser(PurchaserID) on delete cascade on update cascade,  
+	FOREIGN KEY (PurchaserID) references Purchaser(ID) on delete cascade on update cascade,  
 	FOREIGN KEY (ProdNo) references Products(ProdNo) on delete cascade on update cascade
 );
 
 CREATE TABLE FoodOrder(
-    OrderNo         INTEGER(10)        NOT NULL,
+    OrderNo         INTEGER(10)        NOT NULL AUTO_INCREMENT ,
     ProdNo         INTEGER(8)        NOT NULL,
     Quantity_Bought     INTEGER        NOT NULL,
     Date_Sold         DATE            NOT NULL,
@@ -45,8 +55,9 @@ CREATE TABLE FoodOrder(
     SellerID         INTEGER(6)        NOT NULL,
     Delivery_Date     DATE            NOT NULL,
     PRIMARY KEY (OrderNo), 
-	FOREIGN KEY (PurchaserID) references Purchaser(PurchaserID) on delete cascade on update cascade, 
-	FOREIGN KEY (SellerID) references Seller(SellerID) on delete cascade on update cascade
+	FOREIGN KEY (PurchaserID) references Purchaser(ID) on delete cascade on update cascade, 
+	FOREIGN KEY (SellerID) references Seller(ID) on delete cascade on update cascade
 );
+
 
 
